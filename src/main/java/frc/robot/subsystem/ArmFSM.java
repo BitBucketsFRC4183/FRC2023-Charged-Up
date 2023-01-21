f22a40b4dcb9650dbeb3f02f64ad2b52747bc301
WilliamYeohBaxter <wyeohbaxter@gmail.com>
2023-01-20 20:46:14 -0700

    armSubsystem changes

---
+++ src/main/java/frc/robot/subsystem/ArmFSM.java
@@ -0,0 +1,32 @@
+package frc.robot.subsystem;
+
+public enum ArmFSM {
+
+    ENABLED,
+    DISABLED,
+    HIGH,
+    LOW,
+    boolean disableArm = false;
+
+
+    public void teleopPeriodic()
+    {
+            switch (ArmFSM)
+            {
+                case ENABLED:
+                    if (!disableArm) {
+                        // DISABLED:
+                    }
+
+                    case DIASBLED:
+                        if (disableArm) {
+                       // ENABLED;
+
+                    }
+                    case HIGH:
+                        if ()
+        }
+
+
+    }
+}
