5386689e9fbeebfed42912994a2af0822b2ec807
DanielWhatsGood <daniel.changxu.wu@gmail.com>
2023-01-22 15:49:03 -0700

    getHighPosPressed is now used in ArmSubsystem in the switch with ArmFSM

--- src/main/java/org/bitbuckets/arm/ArmControlSetup.java
+++ src/main/java/org/bitbuckets/arm/ArmControlSetup.java
@@ -11,8 +11,8 @@ public class ArmControlSetup implements ISetup<ArmControl> {
     @Override
     public ArmControl build(ProcessPath path) {
 
-       var lowerJoint = new CANSparkMax(0, CANSparkMaxLowLevel.MotorType.kBrushless);
-       var upperJoint = new CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless);
+        var lowerJoint = new CANSparkMax(9, CANSparkMaxLowLevel.MotorType.kBrushless);
+        var upperJoint = new CANSparkMax(3, CANSparkMaxLowLevel.MotorType.kBrushless);
 
         return new ArmControl(lowerJoint, upperJoint);
     }
--- src/main/java/org/bitbuckets/bootstrap/BuildConstants.java
+++ src/main/java/org/bitbuckets/bootstrap/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC2023-Charged-Up";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 36;
-  public static final String GIT_SHA = "d3f3158fb3f625142928ba37d3f092242a9fbe83";
-  public static final String GIT_DATE = "2023-01-20 21:42:39 MST";
+  public static final int GIT_REVISION = 37;
+  public static final String GIT_SHA = "d532b4932468d164e459a702702ed92a006bfb1e";
+  public static final String GIT_DATE = "2023-01-21 21:44:49 MST";
   public static final String GIT_BRANCH = "armPrototype";
-  public static final String BUILD_DATE = "2023-01-21 21:39:38 MST";
-  public static final long BUILD_UNIX_TIME = 1674362378990L;
+  public static final String BUILD_DATE = "2023-01-22 15:42:11 MST";
+  public static final long BUILD_UNIX_TIME = 1674427331782L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
