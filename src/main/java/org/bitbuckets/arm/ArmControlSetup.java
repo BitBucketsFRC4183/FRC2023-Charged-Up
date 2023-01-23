d8a61283744d518ada9762f03faa0c577fdaab35
DanielWhatsGood <daniel.changxu.wu@gmail.com>
2023-01-22 20:02:08 -0700

    Started PID and initiated motor encoders

--- src/main/java/org/bitbuckets/arm/ArmControlSetup.java
+++ src/main/java/org/bitbuckets/arm/ArmControlSetup.java
@@ -14,6 +14,15 @@ public class ArmControlSetup implements ISetup<ArmControl> {
         var lowerJoint = new CANSparkMax(9, CANSparkMaxLowLevel.MotorType.kBrushless);
         var upperJoint = new CANSparkMax(3, CANSparkMaxLowLevel.MotorType.kBrushless);
 
+        lowerJoint.getPIDController().setP(ArmConstants.kP);
+        lowerJoint.getPIDController().setI(ArmConstants.kI);
+        lowerJoint.getPIDController().setD(ArmConstants.kD);
+
+        upperJoint.getPIDController().setP(ArmConstants.kP);
+        upperJoint.getPIDController().setI(ArmConstants.kI);
+        upperJoint.getPIDController().setD(ArmConstants.kD);
+
+
         return new ArmControl(lowerJoint, upperJoint);
     }
 }
--- src/main/java/org/bitbuckets/bootstrap/BuildConstants.java
+++ src/main/java/org/bitbuckets/bootstrap/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC2023-Charged-Up";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 38;
-  public static final String GIT_SHA = "5386689e9fbeebfed42912994a2af0822b2ec807";
-  public static final String GIT_DATE = "2023-01-22 15:49:03 MST";
+  public static final int GIT_REVISION = 39;
+  public static final String GIT_SHA = "346997bf02ca2d80cba03a55ad71e6b05d99ca7c";
+  public static final String GIT_DATE = "2023-01-22 17:32:09 MST";
   public static final String GIT_BRANCH = "armPrototype";
-  public static final String BUILD_DATE = "2023-01-22 17:24:14 MST";
-  public static final long BUILD_UNIX_TIME = 1674433454121L;
+  public static final String BUILD_DATE = "2023-01-22 19:54:41 MST";
+  public static final long BUILD_UNIX_TIME = 1674442481578L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
