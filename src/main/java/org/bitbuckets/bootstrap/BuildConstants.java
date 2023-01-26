886c4cd1fb98c6ac1b575908fabfd0d3526a7719
matt <yuuki@superyuuki.com>
2023-01-25 19:34:11 -0700

    make errors go away

--- src/main/java/org/bitbuckets/bootstrap/BuildConstants.java
+++ src/main/java/org/bitbuckets/bootstrap/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC2023-Charged-Up";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 66;
-  public static final String GIT_SHA = "eee0986c44f3517b2f22df58ed47a136112fd999";
-  public static final String GIT_DATE = "2023-01-25 00:11:55 MST";
+  public static final int GIT_REVISION = 67;
+  public static final String GIT_SHA = "04b2e8891ef46095da62590537c96ac6a343df33";
+  public static final String GIT_DATE = "2023-01-25 00:22:54 MST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2023-01-25 00:22:32 MST";
-  public static final long BUILD_UNIX_TIME = 1674631352748L;
+  public static final String BUILD_DATE = "2023-01-25 19:33:22 MST";
+  public static final long BUILD_UNIX_TIME = 1674700402077L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
