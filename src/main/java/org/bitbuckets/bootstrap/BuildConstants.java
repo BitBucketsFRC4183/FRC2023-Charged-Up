d90dd4b69b8e02c87beb0c79544d61f31545acc2
DanielWhatsGood <daniel.changxu.wu@gmail.com>
2023-01-27 18:43:37 -0700

    Rebase ArmPrototype to main

---
+++ src/main/java/org/bitbuckets/bootstrap/BuildConstants.java
@@ -0,0 +1,19 @@
+package org.bitbuckets.bootstrap;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "FRC2023-Charged-Up";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 87;
+  public static final String GIT_SHA = "4df9a166e0956301987c6d14ea0e9bbcbec4d64b";
+  public static final String GIT_DATE = "2023-01-27 17:57:23 MST";
+  public static final String GIT_BRANCH = "armPrototype";
+  public static final String BUILD_DATE = "2023-01-27 18:41:07 MST";
+  public static final long BUILD_UNIX_TIME = 1674870067587L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
