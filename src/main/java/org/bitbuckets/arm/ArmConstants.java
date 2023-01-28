ba117422db048d698fe413b1aaaa613d4c7daafe
DanielWhatsGood <daniel.changxu.wu@gmail.com>
2023-01-27 21:37:10 -0700

    Started work on Position-based movement ArmControlTest to determine that conversion between mechanism rotation and raw rotation of motor works Commented out old Drive Subsystem in RobotSetup to test ArmSubsystem

--- src/main/java/org/bitbuckets/arm/ArmConstants.java
+++ src/main/java/org/bitbuckets/arm/ArmConstants.java
@@ -7,8 +7,8 @@ public interface ArmConstants {
     // calculated gearRatio
     // Input to output- 5:1 4:1 3:1
     // Final gear different for each arm; 12:26 for lower and 12:30 for upper
-    double lowerArmGearRatio = (5 * 4 * 3) * (12. / 26.);
-    double upperArmGearRation = (5 * 4 * 3) * (12. / 30.);
+    double lowerArmGearRatio = (5 * 4 * 3) * (12. / 26.); //roughly 27.69
+    double upperArmGearRatio = (5 * 4 * 3) * (12. / 30.);
 
     double lowerArmConversionFactor = (5 * 4 * 3) * (12. / 26.) * 360;
     double upperArmConversionFactor = (5 * 4 * 3) * (12. / 30.) * 360;
---
+++ src/test/java/org/bitbuckets/arm/ArmControlTest.java
@@ -0,0 +1,33 @@
+package org.bitbuckets.arm;
+
+import org.bitbuckets.lib.hardware.IMotorController;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+class ArmControlTest {
+
+    IMotorController lowerJoint;
+    IMotorController upperJoint;
+    @BeforeEach
+    public void beforeEach() {
+        lowerJoint = mock();
+        upperJoint = mock();
+    }
+
+    @Test
+    void convertMechanismRotationtoRawRotation_lowerJoint() {
+        ArmControl control = new ArmControl(lowerJoint, upperJoint);
+
+        assertEquals(27.69, control.convertMechanismRotationtoRawRotation_lowerJoint(1), .1);
+    }
+
+    @Test
+    void convertMechanismRotationtoRawRotation_upperJoint() {
+        ArmControl control = new ArmControl(lowerJoint, upperJoint);
+
+        assertEquals(24., control.convertMechanismRotationtoRawRotation_upperJoint(1), .1);
+    }
+}
