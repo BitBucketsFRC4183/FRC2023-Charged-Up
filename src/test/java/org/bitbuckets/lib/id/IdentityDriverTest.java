456412c4a92e210c8a7ae697ec73349a15b318ad
matt <yuuki@superyuuki.com>
2023-01-28 00:01:54 -0700

    fix tests and remove a todo

--- src/test/java/org/bitbuckets/lib/id/IdentityDriverTest.java
+++ src/test/java/org/bitbuckets/lib/id/IdentityDriverTest.java
@@ -10,7 +10,7 @@ public class IdentityDriverTest {
     public void shouldHaveRoot() {
         IdentityDriver manager = new IdentityDriver(); //why was this mocked lmao
 
-        Assertions.assertEquals("root/", manager.fullPath(0));
+        Assertions.assertEquals("", manager.fullPath(0));
     }
 
     @Test
@@ -18,7 +18,7 @@ public class IdentityDriverTest {
         IdentityDriver identityDriver = new IdentityDriver();
 
         int id = identityDriver.childProcess(0, "hello");
-        Assertions.assertEquals("root/hello/", identityDriver.fullPath(id));
+        Assertions.assertEquals("hello/", identityDriver.fullPath(id));
     }
 
 
