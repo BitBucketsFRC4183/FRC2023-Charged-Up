618fef5f77362b0f6db6b59a8d1edd5c3a5c1f05
arjuncaputo <arjuncaputo2025@gmail.com>
2023-01-29 12:43:53 -0700

    Setting up vision pose estimator

--- src/main/java/org/bitbuckets/bootstrap/BuildConstants.java
+++ src/main/java/org/bitbuckets/bootstrap/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC2023-Charged-Up";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 75;
-  public static final String GIT_SHA = "a2d56a36c4db8dc4bf3783c2279165ff3cc44a26";
-  public static final String GIT_DATE = "2023-01-25 19:50:27 MST";
+  public static final int GIT_REVISION = 76;
+  public static final String GIT_SHA = "2dfdbf4e6f448dc930270aefc94e7c8cfb93ab8f";
+  public static final String GIT_DATE = "2023-01-29 01:28:23 MST";
   public static final String GIT_BRANCH = "Vision";
-  public static final String BUILD_DATE = "2023-01-27 21:33:30 MST";
-  public static final long BUILD_UNIX_TIME = 1674880410382L;
+  public static final String BUILD_DATE = "2023-01-29 11:24:54 MST";
+  public static final long BUILD_UNIX_TIME = 1675016694662L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
