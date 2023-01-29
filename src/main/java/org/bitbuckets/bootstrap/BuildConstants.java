cefdbccdede62b42b9471f0dbbebaa86ab78e9b7
fadshfkjdshl <therese.bunyi@icloud.com>
2023-01-28 20:12:20 -0700

    merged main

--- src/main/java/org/bitbuckets/bootstrap/BuildConstants.java
+++ src/main/java/org/bitbuckets/bootstrap/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC2023-Charged-Up";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 112;
-  public static final String GIT_SHA = "55020c600e58694bf0d6e62f4e3fafc073eeb0d4";
-  public static final String GIT_DATE = "2023-01-28 19:55:30 MST";
+  public static final int GIT_REVISION = 136;
+  public static final String GIT_SHA = "3c49c31b7ea5952033f122d3f401f55311629c0f";
+  public static final String GIT_DATE = "2023-01-28 20:11:33 MST";
   public static final String GIT_BRANCH = "armPrototype";
-  public static final String BUILD_DATE = "2023-01-28 20:08:48 MST";
-  public static final long BUILD_UNIX_TIME = 1674961728280L;
+  public static final String BUILD_DATE = "2023-01-28 20:11:56 MST";
+  public static final long BUILD_UNIX_TIME = 1674961916417L;
   public static final int DIRTY = 0;
 
   private BuildConstants(){}
