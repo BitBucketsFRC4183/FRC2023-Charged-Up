be754be8b5133b11b82dc11b98839870bfd239c3
DanielWhatsGood <daniel.changxu.wu@gmail.com>
2023-01-29 21:08:12 -0700

    added documentation for states

--- src/main/java/org/bitbuckets/bootstrap/BuildConstants.java
+++ src/main/java/org/bitbuckets/bootstrap/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC2023-Charged-Up";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 161;
-  public static final String GIT_SHA = "5c07c0aaf383c761e52c7f513321c19f98fe304e";
-  public static final String GIT_DATE = "2023-01-29 20:53:25 MST";
+  public static final int GIT_REVISION = 139;
+  public static final String GIT_SHA = "6a365816519551077be8f04c73756a60246e0036";
+  public static final String GIT_DATE = "2023-01-29 15:21:49 MST";
   public static final String GIT_BRANCH = "armPrototype";
-  public static final String BUILD_DATE = "2023-01-29 20:56:30 MST";
-  public static final long BUILD_UNIX_TIME = 1675050990767L;
+  public static final String BUILD_DATE = "2023-01-29 16:31:26 MST";
+  public static final long BUILD_UNIX_TIME = 1675035086372L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
