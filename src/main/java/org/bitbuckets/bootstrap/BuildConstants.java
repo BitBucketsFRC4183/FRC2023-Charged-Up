ff550f9757f616f9745537bfbcfcc11773901df9
matt <yuuki@superyuuki.com>
2023-02-01 16:48:09 -0700

    more broken stuff

--- src/main/java/org/bitbuckets/bootstrap/BuildConstants.java
+++ src/main/java/org/bitbuckets/bootstrap/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC2023-Charged-Up";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 167;
-  public static final String GIT_SHA = "008becbdf6ce348020c9df84e41381c3d00a2e61";
-  public static final String GIT_DATE = "2023-02-01 13:41:50 MST";
+  public static final int GIT_REVISION = 168;
+  public static final String GIT_SHA = "1ebbd10e70ea553de18894bca21fad0858ec20fb";
+  public static final String GIT_DATE = "2023-02-01 13:42:39 MST";
   public static final String GIT_BRANCH = "mattlib/rolling-0.1.3";
-  public static final String BUILD_DATE = "2023-02-01 13:42:34 MST";
-  public static final long BUILD_UNIX_TIME = 1675284154518L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2023-02-01 13:43:23 MST";
+  public static final long BUILD_UNIX_TIME = 1675284203605L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
