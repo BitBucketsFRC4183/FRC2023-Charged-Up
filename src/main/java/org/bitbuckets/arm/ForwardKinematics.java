f4592f83a75f4156046633605f80a43099629887
fadshfkjdshl <therese.bunyi@icloud.com>
2023-02-01 18:12:23 -0700

    foward kinematics class

--- src/main/java/org/bitbuckets/arm/ForwardKinematics.java
+++ src/main/java/org/bitbuckets/arm/ForwardKinematics.java
@@ -1,4 +1,33 @@
 package org.bitbuckets.arm;
 
 public class ForwardKinematics {
+    final double theta1;
+    final double theta2;
+
+    public ForwardKinematics(double theta1, double theta2) {
+        this.theta1 = theta1;
+        this.theta2 = theta2;
+
+    }
+
+
+    public double getX() {
+
+        double z = Math.sqrt(Math.pow(ArmConstants.lowerJointLength, 2) + Math.pow(ArmConstants.upperJointLength, 2) + (2 * ArmConstants.lowerJointLength * ArmConstants.upperJointLength * Math.cos(180 - theta2)));
+        double beta = Math.acos((Math.pow(z, 2) + Math.pow(ArmConstants.lowerJointLength, 2) - Math.pow(ArmConstants.upperJointLength, 2)) / (2 * ArmConstants.lowerJointLength * z));
+        double x = z * Math.cos(theta1 - beta);
+
+        return x;
+    }
+
+
+    public double getY() {
+
+        double z = Math.sqrt(Math.pow(ArmConstants.lowerJointLength, 2) + Math.pow(ArmConstants.upperJointLength, 2) + (2 * ArmConstants.lowerJointLength * ArmConstants.upperJointLength * Math.cos(180 - theta2)));
+        double beta = Math.acos((Math.pow(z, 2) + Math.pow(ArmConstants.lowerJointLength, 2) - Math.pow(ArmConstants.upperJointLength, 2)) / (2 * ArmConstants.lowerJointLength * z));
+        double y = z * Math.sin(theta1 - beta);
+
+        return y;
+    }
 }
+
