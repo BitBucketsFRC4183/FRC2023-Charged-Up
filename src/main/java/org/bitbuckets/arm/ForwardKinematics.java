a403d0451567d47b134a792ee8d5f39937803620
fadshfkjdshl <therese.bunyi@icloud.com>
2023-02-01 18:57:28 -0700

    forward kinematics test (works)

--- src/main/java/org/bitbuckets/arm/ForwardKinematics.java
+++ src/main/java/org/bitbuckets/arm/ForwardKinematics.java
@@ -4,6 +4,7 @@ public class ForwardKinematics {
     final double theta1;
     final double theta2;
 
+    // Parameters must be in radians
     public ForwardKinematics(double theta1, double theta2) {
         this.theta1 = theta1;
         this.theta2 = theta2;
@@ -13,7 +14,7 @@ public class ForwardKinematics {
 
     public double getX() {
 
-        double z = Math.sqrt(Math.pow(ArmConstants.lowerJointLength, 2) + Math.pow(ArmConstants.upperJointLength, 2) + (2 * ArmConstants.lowerJointLength * ArmConstants.upperJointLength * Math.cos(180 - theta2)));
+        double z = Math.sqrt(Math.pow(ArmConstants.lowerJointLength, 2) + Math.pow(ArmConstants.upperJointLength, 2) + (2 * ArmConstants.lowerJointLength * ArmConstants.upperJointLength * Math.cos(theta2)));
         double beta = Math.acos((Math.pow(z, 2) + Math.pow(ArmConstants.lowerJointLength, 2) - Math.pow(ArmConstants.upperJointLength, 2)) / (2 * ArmConstants.lowerJointLength * z));
         double x = z * Math.cos(theta1 - beta);
 
@@ -23,7 +24,7 @@ public class ForwardKinematics {
 
     public double getY() {
 
-        double z = Math.sqrt(Math.pow(ArmConstants.lowerJointLength, 2) + Math.pow(ArmConstants.upperJointLength, 2) + (2 * ArmConstants.lowerJointLength * ArmConstants.upperJointLength * Math.cos(180 - theta2)));
+        double z = Math.sqrt(Math.pow(ArmConstants.lowerJointLength, 2) + Math.pow(ArmConstants.upperJointLength, 2) + (2 * ArmConstants.lowerJointLength * ArmConstants.upperJointLength * Math.cos(theta2)));
         double beta = Math.acos((Math.pow(z, 2) + Math.pow(ArmConstants.lowerJointLength, 2) - Math.pow(ArmConstants.upperJointLength, 2)) / (2 * ArmConstants.lowerJointLength * z));
         double y = z * Math.sin(theta1 - beta);
 
