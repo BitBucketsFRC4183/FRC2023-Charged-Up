adc588ca7b6a788d1f63dc2723348ca3ff4fdc84
matt <yuuki@superyuuki.com>
2023-02-02 14:56:19 -0700

    pidcalculator works

--- src/test/java/org/bitbuckets/lib/control/PIDCalculatorSetupTest.java
+++ src/test/java/org/bitbuckets/lib/control/PIDCalculatorSetupTest.java
@@ -2,13 +2,16 @@ package org.bitbuckets.lib.control;
 
 import edu.wpi.first.hal.HAL;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import org.bitbuckets.lib.ProcessPath;
-import org.bitbuckets.lib.ProcessPathUtil;
+import org.bitbuckets.lib.core.IdentityDriver;
+import org.bitbuckets.lib.core.LoopDriver;
+import org.bitbuckets.lib.tune.TuneableDriver;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 class PIDCalculatorSetupTest {
 
@@ -16,23 +19,33 @@ class PIDCalculatorSetupTest {
     void pidCalculator_shouldWork() throws InterruptedException {
         assert HAL.initialize(500, 0);
 
-        ProcessPath path = ProcessPathUtil.testingProcessPath();
+        NetworkTable table = NetworkTableInstance.getDefault().getTable("RealOutputs/MattTuneables");
+
+        IdentityDriver id = new IdentityDriver();
+        TuneableDriver driver = new TuneableDriver(table, id);
+        LoopDriver loopDriver = new LoopDriver();
+
+        ProcessPath path = new ProcessPath(
+                0,
+                Mockito.mock(),
+                id,
+                Mockito.mock(),
+                loopDriver,
+                driver,
+                false
+        );
+
         IPIDCalculator calculator = new PIDCalculatorSetup(
                 new PIDConfig(1,0,0,0)
         ).build(path.addChild("calculator"));
 
-        Assertions.assertTrue(NetworkTableInstance.getDefault().getTable("RealOutputs/MattTuneables").getEntry("calculator").exists());
-
-        NetworkTableEntry e = NetworkTableInstance.getDefault().getTable("RealOutputs/MattTuneables").getEntry("calculator");
-
-        Assertions.assertTrue(e.exists(), "nettable entry should exist");
-        Assertions.assertTrue(e.isValid(), "nettable entry should be valid");
-
+        Assertions.assertTrue(table.getEntry("calculator/p").exists());
         Assertions.assertEquals(1.0, calculator.rawAccess(PIDController.class).getP(), "initial P value should be 1");
 
-        NetworkTableInstance.getDefault().getTable("RealOutputs/MattTuneables").getEntry("calculator/p").setDouble(2.0);
+        table.getEntry("calculator/p").setDouble(2.0);
 
         Thread.sleep(1000);
+        loopDriver.runPeriodic();
         Assertions.assertEquals(2.0, calculator.rawAccess(PIDController.class).getP(), "pid calculator should read 2");
 
     }
