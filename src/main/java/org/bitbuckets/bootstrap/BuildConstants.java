a26b0eb0a057ff126f0eb76d0ab19dcbf8a97727
Ibrahim Bah <ib.bah2005@gmail.com>
2023-02-01 19:07:31 -0700

    getting code to work to prepare for the thanos snap
    got rid of DriveSDSSubsystem

--- src/main/java/org/bitbuckets/bootstrap/BuildConstants.java
+++ src/main/java/org/bitbuckets/bootstrap/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC2023-Charged-Up";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 132;
-  public static final String GIT_SHA = "778aa0f2c1f29dd7258b4fbab425c4fe2ae933cb";
-  public static final String GIT_DATE = "2023-01-29 19:50:58 MST";
-  public static final String GIT_BRANCH = "Vision";
-  public static final String BUILD_DATE = "2023-01-29 20:04:30 MST";
-  public static final long BUILD_UNIX_TIME = 1675047870096L;
+  public static final int GIT_REVISION = 159;
+  public static final String GIT_SHA = "8dbef92d459431f6dc182e6d1f3b31b60a17b11d";
+  public static final String GIT_DATE = "2023-02-01 18:46:23 MST";
+  public static final String GIT_BRANCH = "autoPathPlanning2";
+  public static final String BUILD_DATE = "2023-02-01 19:06:28 MST";
+  public static final long BUILD_UNIX_TIME = 1675303588498L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
