cb3b1ee73bb83c2a68d3a27bb02c3cb3e1034608
matt <yuuki@superyuuki.com>
2023-02-04 19:49:44 -0700

    hopefully fix drive constants

--- src/main/java/org/bitbuckets/bootstrap/BuildConstants.java
+++ src/main/java/org/bitbuckets/bootstrap/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC2023-Charged-Up";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 288;
-  public static final String GIT_SHA = "ada10b80a8630e3cb58d982ef2cc27eeff2f7775";
-  public static final String GIT_DATE = "2023-02-04 19:53:50 MST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2023-02-04 20:39:44 MST";
-  public static final long BUILD_UNIX_TIME = 1675568384990L;
+  public static final int GIT_REVISION = 292;
+  public static final String GIT_SHA = "6a1bf186c4c92c19b586d8db384c4fd0188c0f9f";
+  public static final String GIT_DATE = "2023-02-04 23:10:35 MST";
+  public static final String GIT_BRANCH = "visiony";
+  public static final String BUILD_DATE = "2023-02-04 23:12:43 MST";
+  public static final long BUILD_UNIX_TIME = 1675577563937L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
