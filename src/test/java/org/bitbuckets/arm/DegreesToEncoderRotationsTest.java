f2b5ba59be00393f839ddb8a1dfe757c4bb9d2d6
Kai Koehler <codekai16@gmail.com>
2023-02-08 20:07:49 -0700

    Degrees to encoder rotations test commented out
    This test is on main and is broken. please make a branch and fix it

--- src/test/java/org/bitbuckets/arm/DegreesToEncoderRotationsTest.java
+++ src/test/java/org/bitbuckets/arm/DegreesToEncoderRotationsTest.java
@@ -8,16 +8,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DegreesToEncoderRotationsTest {
 
-    final IMotorController lowerJoint;
-    final IMotorController upperJoint;
-
-    public DegreesToEncoderRotationsTest(IMotorController lowerJoint, IMotorController upperJoint) {
-        this.lowerJoint = lowerJoint;
-        this.upperJoint = upperJoint;
-    }
-
-    @Test
-    public void name() {
-        assertEquals(0, new ArmControl(lowerJoint, upperJoint, Mockito.mock()).convertMechanismRotationtoRawRotation_lowerJoint(180));
-    }
+//    final IMotorController lowerJoint;
+//    final IMotorController upperJoint;
+//
+//    public DegreesToEncoderRotationsTest(IMotorController lowerJoint, IMotorController upperJoint) {
+//        this.lowerJoint = lowerJoint;
+//        this.upperJoint = upperJoint;
+//    }
+//
+//    @Test
+//    public void name() {
+//        assertEquals(0, new ArmControl(lowerJoint, upperJoint, Mockito.mock()).convertMechanismRotationtoRawRotation_lowerJoint(180));
+//    }
 }
