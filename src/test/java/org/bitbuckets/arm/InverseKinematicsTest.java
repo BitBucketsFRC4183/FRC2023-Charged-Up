d1491b6be8cd4f1d0ef16ff921045029e6e84bd8
Daniel <105016190+DanielWhatsGood@users.noreply.github.com>
2023-02-10 19:55:14 -0700

    Arm fsm testing69 (#139)
    * New branch
    
    * New branch
    
    * Changes by zINKY dOO Doo
    
    * Deleted BuildConstants
    
    * Added inverseKinematicsTests for arm states
    
    * Added one more IK test
    
    * Changed from face buttons to DPad
    Started troubleshooting PID and InverseKinematics issues
    
    * Changes by scotty
    
    * Changes by everyone
    
    * Changes by yuuki
    
    * Changes by zINKY dOO Doo
    
    * Added LOW_NODE_X and LOW_NODE_Y
    
    * Added LOW_NODE_X and LOW_NODE_Y
    
    * Made everything either in degrees or mechanism rotations
    Fixed some bugs
    
    * Changes by scotty
    
    * Changes by william YB
    
    * Disabled some tests that were supposed to fail
    
    ---------
    
    Co-authored-by: Craig Post <cpost@bitbuckets.org>
    Co-authored-by: matt <yuuki@superyuuki.com>
    Co-authored-by: Kai Koehler <codekai16@gmail.com>

--- src/test/java/org/bitbuckets/arm/InverseKinematicsTest.java
+++ src/test/java/org/bitbuckets/arm/InverseKinematicsTest.java
@@ -4,18 +4,34 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class InverseKinematicsTest {
 
     @Disabled
     @Test
     void getLowerJointAngle() {
-        assertEquals(Double.NaN, new InverseKinematics(2, 2).getLowerJointAngle());
+        assertEquals(Double.NaN, new InverseKinematics(ArmConstants.HUMAN_INTAKE_X, ArmConstants.HUMAN_INTAKE_Y).getLowerJoint_degrees());
+        assertEquals(Double.NaN, new InverseKinematics(ArmConstants.PREPARE_X, ArmConstants.PREPARE_Y).getLowerJoint_degrees());
+        assertEquals(Double.NaN, new InverseKinematics(ArmConstants.STORAGE_X, ArmConstants.STORAGE_Y).getLowerJoint_degrees());
+        assertEquals(Double.NaN, new InverseKinematics(ArmConstants.HIGH_NODE_X, ArmConstants.HIGH_NODE_Y).getLowerJoint_degrees());
+        assertEquals(Double.NaN, new InverseKinematics(ArmConstants.MID_NODE_X, ArmConstants.MID_NODE_Y).getLowerJoint_degrees());
+        assertEquals(Double.NaN, new InverseKinematics(ArmConstants.LOW_NODE_X, ArmConstants.LOW_NODE_Y).getLowerJoint_degrees());
+
+
+
     }
 
     @Disabled
     @Test
     void getUpperJointAngle() {
-        assertEquals(Double.NaN, new InverseKinematics(2, 2).getUpperJointAngle());
+
+        assertEquals(Double.NaN, new InverseKinematics(ArmConstants.HUMAN_INTAKE_X, ArmConstants.HUMAN_INTAKE_Y).getUpperJoint_degrees());
+        assertEquals(Double.NaN, new InverseKinematics(ArmConstants.PREPARE_X, ArmConstants.PREPARE_Y).getUpperJoint_degrees());
+        assertEquals(Double.NaN, new InverseKinematics(ArmConstants.STORAGE_X, ArmConstants.STORAGE_Y).getUpperJoint_degrees());
+        assertEquals(Double.NaN, new InverseKinematics(ArmConstants.HIGH_NODE_X, ArmConstants.HIGH_NODE_Y).getUpperJoint_degrees());
+        assertEquals(Double.NaN, new InverseKinematics(ArmConstants.MID_NODE_X, ArmConstants.MID_NODE_Y).getUpperJoint_degrees());
+        assertEquals(Double.NaN, new InverseKinematics(ArmConstants.LOW_NODE_X, ArmConstants.LOW_NODE_Y).getUpperJoint_degrees());
+
     }
 }
