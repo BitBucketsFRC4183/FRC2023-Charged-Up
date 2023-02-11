85d0eef044032a943ffbbb3e08ffd5887e497d92
DanielWhatsGood <daniel.changxu.wu@gmail.com>
2023-02-10 22:05:54 -0700

    Changes by matt

--- src/main/java/org/bitbuckets/arm/ArmSubsystemSetup.java
+++ src/main/java/org/bitbuckets/arm/ArmSubsystemSetup.java
@@ -8,6 +8,7 @@ import org.bitbuckets.arm.sim.SimArm;
 import org.bitbuckets.arm.sim.SimArmSetup;
 import org.bitbuckets.lib.ISetup;
 import org.bitbuckets.lib.ProcessPath;
+import org.bitbuckets.lib.hardware.IMotorController;
 import org.bitbuckets.lib.log.Debuggable;
 import org.bitbuckets.lib.util.MockingUtil;
 import org.bitbuckets.lib.vendor.spark.SparkSetup;
@@ -26,9 +27,21 @@ public class ArmSubsystemSetup implements ISetup<ArmSubsystem> {
             return MockingUtil.buddy(ArmSubsystem.class);
         }
 
+        ISetup<IMotorController> lowerArm;
+        ISetup<IMotorController> upperArm;
+
+        if (self.isReal()) {
+            lowerArm = new SparkSetup(9, ArmConstants.LOWER_CONFIG, ArmConstants.LOWER_PID);
+            upperArm = new SparkSetup(10, ArmConstants.UPPER_CONFIG, ArmConstants.UPPER_PID);
+        } else {
+
+            lowerArm = new SimArmSetup(ArmConstants.LOWER_CONFIG, new ArmConfig(ArmConstants.LOWER_JOINT_LENGTH, 1, 1, 1,true), ArmConstants.LOWER_PID);
+            upperArm = new SimArmSetup(ArmConstants.UPPER_CONFIG, new ArmConfig(ArmConstants.UPPER_JOINT_LENGTH, 1, 1, 1,true), ArmConstants.UPPER_PID);
+        }
+
         ArmControlSetup armControlSetup = new ArmControlSetup(
-                new SparkSetup(9, ArmConstants.LOWER_CONFIG, ArmConstants.LOWER_PID),
-                new SparkSetup(10, ArmConstants.UPPER_CONFIG, ArmConstants.UPPER_PID)
+                lowerArm,
+                upperArm
         );
 
         ArmControl armControl = armControlSetup.build(self.addChild("arm-control"));
