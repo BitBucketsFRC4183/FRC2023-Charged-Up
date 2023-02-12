9720834eb6971a8c0aab1e29e84240f470f975c7
Craig Post <cpost@bitbuckets.org>
2023-02-11 18:50:48 -0700

    Added more ArmControl tests

--- src/test/java/org/bitbuckets/arm/ArmControlTest.java
+++ src/test/java/org/bitbuckets/arm/ArmControlTest.java
@@ -1,38 +1,110 @@
 package org.bitbuckets.arm;
 
 import org.bitbuckets.lib.hardware.IMotorController;
+import org.bitbuckets.lib.log.Debuggable;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class ArmControlTest {
+
+    IMotorController lowerJoint;
+    IMotorController lowerJoint1;
+    IMotorController upperJoint;
+    Debuggable debuggable;
+
+    ArmControl control;
+
+    @BeforeEach
+    public void beforeEach() {
+        lowerJoint = mock(IMotorController.class);
+        lowerJoint1 = mock(IMotorController.class);
+        upperJoint = mock(IMotorController.class);
+        debuggable = mock(Debuggable.class);
+
+        control = new ArmControl(lowerJoint, lowerJoint1, upperJoint, debuggable);
+    }
 
 
     @Test
     void convertMechanismRotationToRawRotation_lowerJoint() {
-        ArmControl control = new ArmControl(Mockito.mock(), Mockito.mock(), Mockito.mock(), Mockito.mock());
-
         assertEquals(27.69, control.convertMechanismRotationtoRawRotation_lowerJoint(1), .1);
     }
 
     @Test
     void convertMechanismRotationToRawRotation_upperJoint() {
-        ArmControl control = new ArmControl(Mockito.mock(), Mockito.mock(), Mockito.mock(), Mockito.mock());
-
         assertEquals(24., control.convertMechanismRotationtoRawRotation_upperJoint(1), .1);
     }
 
+    @Test
+    void isReachable() {
+        assertTrue(control.isReachable(0, 0));
+        assertFalse(control.isReachable(0, Double.NaN));
+        assertFalse(control.isReachable(0, Double.NaN));
+        assertFalse(control.isReachable(Double.NaN, Double.NaN));
+    }
 
     @Test
-    void test() {
-        IMotorController lowerJointSpy = Mockito.mock();
+    void isErrorSmallEnough() {
+        when(lowerJoint.getError_mechanismRotations()).thenReturn(1d);
+        when(upperJoint.getError_mechanismRotations()).thenReturn(1d);
+        assertFalse(control.isErrorSmallEnough(.1));
+        assertTrue(control.isErrorSmallEnough(2));
 
-        ArmControl control = new ArmControl(lowerJointSpy, Mockito.mock(), Mockito.mock(), Mockito.mock());
-        control.lowerJoint.moveToPosition_mechanismRotations(control.convertDegreesToRotation(90));
+        // Should work
+//        when(lowerJoint.getError_mechanismRotations()).thenReturn(-1d);
+//        when(upperJoint.getError_mechanismRotations()).thenReturn(-1d);
+//        assertFalse(control.isErrorSmallEnough(.1));
+//        assertTrue(control.isErrorSmallEnough(2));
+    }
 
+    @Test
+    void humanIntake() {
+        // we should move the mechanism
+        control.humanIntake();
+        verify(lowerJoint).moveToPosition_mechanismRotations(anyDouble());
+        verify(upperJoint).moveToPosition_mechanismRotations(anyDouble());
+    }
 
-        Mockito.verify(lowerJointSpy).moveToPosition_mechanismRotations(0.25);
+    @Test
+    void storeArm() {
+        // we should move the mechanism
+        control.humanIntake();
+        verify(lowerJoint).moveToPosition_mechanismRotations(anyDouble());
+        verify(upperJoint).moveToPosition_mechanismRotations(anyDouble());
+    }
 
+    @Test
+    void prepareArm() {
+        // we should move the mechanism
+        control.humanIntake();
+        verify(lowerJoint).moveToPosition_mechanismRotations(anyDouble());
+        verify(upperJoint).moveToPosition_mechanismRotations(anyDouble());
+    }
+
+    @Test
+    void scoreLow() {
+        // we should move the mechanism
+        control.humanIntake();
+        verify(lowerJoint).moveToPosition_mechanismRotations(anyDouble());
+        verify(upperJoint).moveToPosition_mechanismRotations(anyDouble());
+    }
+
+    @Test
+    void scoreMid() {
+        // we should move the mechanism
+        control.humanIntake();
+        verify(lowerJoint).moveToPosition_mechanismRotations(anyDouble());
+        verify(upperJoint).moveToPosition_mechanismRotations(anyDouble());
+    }
+
+    @Test
+    void scoreHigh() {
+        // we should move the mechanism
+        control.humanIntake();
+        verify(lowerJoint).moveToPosition_mechanismRotations(anyDouble());
+        verify(upperJoint).moveToPosition_mechanismRotations(anyDouble());
     }
 }
