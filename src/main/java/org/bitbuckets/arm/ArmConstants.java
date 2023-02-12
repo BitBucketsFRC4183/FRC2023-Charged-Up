50e35b18f6211821de35a6e2d60a3c7e7ac5e80d
matt <yuuki@superyuuki.com>
2023-02-12 03:00:16 -0700

    fix another dumb unit error

--- src/main/java/org/bitbuckets/arm/ArmConstants.java
+++ src/main/java/org/bitbuckets/arm/ArmConstants.java
@@ -63,8 +63,8 @@ public interface ArmConstants {
     PIDConfig LOWER_PID = new PIDConfig(0.3, 0, 0, 0);
     PIDConfig UPPER_PID = new PIDConfig(0.3, 0, 0, 0);
 
-    PIDConfig LOWER_SIMPID = new PIDConfig(0, 0, 0, 0);
-    PIDConfig UPPER_SIMPID = new PIDConfig(0, 0, 0, 0);
+    PIDConfig LOWER_SIMPID = new PIDConfig(0.3, 0, 0, 0);
+    PIDConfig UPPER_SIMPID = new PIDConfig(0.3, 0, 0, 0);
 
     ArmConfig LOWER_ARM = new ArmConfig(
             Units.inchesToMeters(27),
