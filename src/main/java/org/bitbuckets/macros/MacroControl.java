728c434a2fdeaaba9bfb335eb44c09979ddd7b84
Kai Koehler <45439844+Fireye04@users.noreply.github.com>
2023-02-12 19:25:44 -0800

    Macros (#155)
    * Macro Init
    
    * Initialize the Macro subsytem
    
    * Initialize MacroSubsystem
    
    * Complete macro subsystem initialization
    
    * import fixes

---
+++ src/main/java/org/bitbuckets/macros/MacroControl.java
@@ -0,0 +1,14 @@
+package org.bitbuckets.macros;
+
+import org.bitbuckets.lib.log.Debuggable;
+
+public class MacroControl {
+
+    final Debuggable debuggable;
+
+    public MacroControl(Debuggable debuggable) {
+        this.debuggable = debuggable;
+    }
+
+
+}
