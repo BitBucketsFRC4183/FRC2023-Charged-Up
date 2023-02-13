baf7870c4af69a61c9c77af37e1169b043ee8cec
Kai Koehler <codekai16@gmail.com>
2023-02-12 20:44:40 -0700

    arm and drive control into macros

--- src/main/java/org/bitbuckets/macros/MacroSubsystem.java
+++ src/main/java/org/bitbuckets/macros/MacroSubsystem.java
@@ -1,18 +1,21 @@
 package org.bitbuckets.macros;
 
+import org.bitbuckets.arm.ArmControl;
+import org.bitbuckets.arm.ArmControlSetup;
+import org.bitbuckets.arm.ArmSubsystem;
 import org.bitbuckets.lib.log.Debuggable;
 
 public class MacroSubsystem {
 
     final MacroInput macroInput;
-    final MacroControl macroControl;
     final Debuggable debuggable;
+    final ArmControl armControl;
 
     MacroFSM state = MacroFSM.DISABLED;
 
-    public MacroSubsystem(MacroInput macroInput, MacroControl macroControl, Debuggable debuggable) {
+    public MacroSubsystem(MacroInput macroInput, ArmControl armControl, Debuggable debuggable) {
         this.macroInput = macroInput;
-        this.macroControl = macroControl;
+        this.armControl = armControl;
         this.debuggable = debuggable;
     }
 
--- src/main/java/org/bitbuckets/macros/MacroSubsystemSetup.java
+++ src/main/java/org/bitbuckets/macros/MacroSubsystemSetup.java
@@ -1,10 +1,16 @@
 package org.bitbuckets.macros;
 
 import edu.wpi.first.wpilibj.Joystick;
+import org.bitbuckets.arm.ArmConstants;
+import org.bitbuckets.arm.ArmControl;
+import org.bitbuckets.arm.ArmControlSetup;
+import org.bitbuckets.drive.controlsds.DriveControl;
+import org.bitbuckets.drive.controlsds.DriveControlSetup;
 import org.bitbuckets.lib.ISetup;
 import org.bitbuckets.lib.ProcessPath;
 import org.bitbuckets.lib.log.Debuggable;
 import org.bitbuckets.lib.util.MockingUtil;
+import org.bitbuckets.lib.vendor.spark.SparkSetup;
 
 public class MacroSubsystemSetup implements ISetup<MacroSubsystem> {
 
@@ -20,13 +26,21 @@ public class MacroSubsystemSetup implements ISetup<MacroSubsystem> {
             return MockingUtil.buddy(MacroSubsystem.class);
         }
 
-        MacroControlSetup macroControlSetup = new MacroControlSetup();
-
-        MacroControl macroControl = macroControlSetup.build(self.addChild("arm-control"));
         MacroInput macroInput = new MacroInput(new Joystick(1), self.generateDebugger());
+
+        ArmControlSetup armControlSetup = new ArmControlSetup(
+                new SparkSetup(11, ArmConstants.LOWER_CONFIG, ArmConstants.LOWER_PID),
+                new SparkSetup(9, ArmConstants.LOWER_CONFIG1, ArmConstants.LOWER_PID),
+                new SparkSetup(10, ArmConstants.UPPER_CONFIG, ArmConstants.UPPER_PID)
+        );
+
+        DriveControlSetup driveControlSetup = new DriveControlSetup()
+
+        ArmControl armControl = armControlSetup.build(self.addChild("arm-control"));
+
         Debuggable debuggable = self.generateDebugger();
 
-        return new MacroSubsystem(macroInput, macroControl, debuggable);
+        return new MacroSubsystem(macroInput, armControl, driveControl, debuggable);
 
 
     }
