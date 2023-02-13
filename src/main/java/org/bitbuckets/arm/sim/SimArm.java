46bf6b240794f8805339bf5698aa5d054dce9a67
Matt Lui <92664603+superyuuki@users.noreply.github.com>
2023-02-13 11:31:12 -0700

    Double jointed arm sim (#156)
    * Fixed issue with ArmFSM
    
    * Changed delta to something more reasonable
    
    * Made SimArm and SimArmSetup
    
    * Changes by matt
    
    * Moved ForwardKinematics and InverseKinematics to the new Kinematics package in arm
    Code still crashes when attempting to simulateJava due to ArmSim failing to initialize
    
    * Worked on ForwardKinematics
    Changed ArmConstants to fit new arm
    Worked on making armSim
    
    * Changes by scotty
    
    * Changes by theresb
    
    * Working on getting ArmSim to actually simulate motors instead of just incrementing angles
    
    * Changes by scotty
    
    * Changes by yuuki
    
    * arm physics, sans gravity
    
    * hack gearing for jointed arm sim to work
    
    * fix another dumb unit error
    
    * fix motor controller error bug
    
    * fix pid
    
    * pidiotic changes
    
    * arm stuff
    
    * potential fixes
    
    * scott: changed stuff in inverse kinematics test to make it work, also added function for commanding pid to stop and go to another state
    therese/william: changed stuff in isReachable(),
    
    * testing and removed setpoint and actual position to calculate error
    
    * arm stuff
    
    * loggables documenting lowerJoint and upperJoint error for future tuning of arm. Simplifications of armInput
    
    * changed mechanismstorotations to get voltage
    
    * pass tests
    
    * remove ass
    
    ---------
    
    Co-authored-by: DanielWhatsGood <daniel.changxu.wu@gmail.com>
    Co-authored-by: Kai Koehler <codekai16@gmail.com>
    Co-authored-by: Scott Song <ScottSong2020@gmail.com>
    Co-authored-by: WilliamYeohBaxter <wyeohbaxter@gmail.com>
    Co-authored-by: fadshfkjdshl <therese.bunyi@icloud.com>

---
+++ src/main/java/org/bitbuckets/arm/sim/SimArm.java
@@ -0,0 +1,128 @@
+package org.bitbuckets.arm.sim;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
+import org.bitbuckets.arm.ArmConstants;
+import org.bitbuckets.lib.hardware.IMotorController;
+import org.bitbuckets.lib.hardware.MotorConfig;
+import org.bitbuckets.lib.log.Debuggable;
+
+public class SimArm implements IMotorController {
+
+    final Debuggable debuggable;
+    final MechanismLigament2d ligament2d;
+    final MotorConfig motorConfig;
+    final SingleJointedArmSim sim;
+    final PIDController armPositionPid;
+
+    double setpoint_mechanismPos = 0;
+
+    public SimArm(Debuggable debuggable, MechanismLigament2d ligament2d, MotorConfig motorConfig, SingleJointedArmSim sim, PIDController armPositionPid) {
+        this.debuggable = debuggable;
+        this.ligament2d = ligament2d;
+        this.motorConfig = motorConfig;
+        this.sim = sim;
+        this.armPositionPid = armPositionPid;
+        //armPositionPid.enableContinuousInput(-1.0, 1.0);
+    }
+
+    @Override
+    public double getMechanismFactor() {
+        return 1;
+    }
+
+    @Override
+    public double getRotationsToMetersFactor() {
+        return motorConfig.rotationToMeterCoefficient;
+    }
+
+    @Override
+    public double getRawToRotationsFactor() {
+        return 1;
+    }
+
+    @Override
+    public double getTimeFactor() {
+        return motorConfig.timeCoefficient;
+    }
+
+    @Override
+    public double getPositionRaw() {
+        return sim.getAngleRads() / Math.PI / 2.0;
+    }
+
+    @Override
+    public double getVelocityRaw() {
+        return sim.getVelocityRadPerSec() / Math.PI / 2.0;
+    }
+
+    @Override
+    public void forceOffset(double offsetUnits_baseUnits) {
+        throw new UnsupportedOperationException("69");
+    }
+
+    @Override
+    public void forceOffset_mechanismRotations(double offsetUnits_mechanismRotations) {
+        throw new UnsupportedOperationException("69");
+    }
+
+    @Override
+    public void moveAtVoltage(double voltage) {
+        cachedInputVoltage = voltage;
+    }
+
+    @Override
+    public void moveAtPercent(double percent) {
+        cachedInputVoltage = percent * 12.0;
+    }
+
+    @Override
+    public void moveToPosition(double setpoint_encoderRotations) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void moveToPosition_mechanismRotations(double position_mechanismRotations) {
+        double currentPosition_mechanismRotations = sim.getAngleRads() / Math.PI / 2.0;
+
+        cachedInputVoltage = armPositionPid.calculate(currentPosition_mechanismRotations, position_mechanismRotations);
+    }
+
+    @Override
+    public void moveAtVelocity(double velocity_encoderMetersPerSecond) {
+        throw new UnsupportedOperationException("does not support");
+    }
+
+    @Override
+    public double getSetpoint_mechanismRotations() {
+        return setpoint_mechanismPos;
+    }
+
+    @Override
+    public <T> T rawAccess(Class<T> clazz) throws UnsupportedOperationException {
+        throw new UnsupportedOperationException("bad");
+    }
+
+    double cachedInputVoltage = 0;
+
+    public void runSimulationLoop() {
+        sim.setInputVoltage(cachedInputVoltage);
+        sim.update(0.02);
+
+        double deg = Units.radiansToDegrees(sim.getAngleRads());
+
+        double current = sim.getAngleRads() / Math.PI / 2.0;
+        double setpoint = armPositionPid.getSetpoint();
+        double error = armPositionPid.getPositionError();
+
+        debuggable.log("error", error);
+        debuggable.log("setpoint", setpoint);
+        debuggable.log("current", current);
+
+        sim.getCurrentDrawAmps();
+
+        ligament2d.setAngle( deg  );
+    }
+}
