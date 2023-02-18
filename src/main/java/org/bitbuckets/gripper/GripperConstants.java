96d25f376581f19e0d7563e12b02dfa1d610a022
Nitish Bhamidipati <nitishbhamidipati@gmail.com>
2023-02-17 18:59:33 -0700

    More gripper state stuff

--- src/main/java/org/bitbuckets/gripper/GripperConstants.java
+++ src/main/java/org/bitbuckets/gripper/GripperConstants.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 
 public interface GripperConstants {
 
-    MotorConfig GRIPPER_CONFIG = new MotorConfig(1, 1, 1, false, true, 10, false, false, Optional.empty());
+    MotorConfig GRIPPER_CONFIG = new MotorConfig(1, 1, 1, false, true, 10, true, true, Optional.empty());
 
     PIDConfig GRIPPER_PID = new PIDConfig(1,0,0,0);
 
