13f58abf83129b8b5b982306740d150ad59a72c0
cpostbitbuckets <43726877+cpostbitbuckets@users.noreply.github.com>
2023-02-18 17:15:39 -0700

    Auto fixes from CRUSH (#172)
    * idk test stuff auto fix gyro
    
    * auto
    
    * stuff that again doesn't work
    
    * worj
    
    * test
    
    * actually log
    
    * Updated pathplanner lib
    
    * path
    
    * actually log
    
    * crpa
    
    * Fixed None path
    
    Updated test path to use whole number units
    
    ---------
    
    Co-authored-by: Vasu <vvkedia007@gmail.com>
    Co-authored-by: matt <yuuki@superyuuki.com>

--- src/main/java/org/bitbuckets/lib/control/ProfiledPIDFSetup.java
+++ src/main/java/org/bitbuckets/lib/control/ProfiledPIDFSetup.java
@@ -7,21 +7,30 @@ import org.bitbuckets.lib.tune.IValueTuner;
 
 public class ProfiledPIDFSetup implements ISetup<IPIDCalculator> {
 
+    final PIDConfig pidConfig;
+    final TrapezoidProfile.Constraints profile;
+
+    public ProfiledPIDFSetup(PIDConfig pidConfig, TrapezoidProfile.Constraints profile) {
+        this.pidConfig = pidConfig;
+        this.profile = profile;
+    }
 
     @Override
     public IPIDCalculator build(ProcessPath self) {
 
         IValueTuner<double[]> tunerForAllValues = self.generateMultiTuner(
                 new String[]{"p", "i", "d", "kV", "kA"},
-                new double[]{0.0, 0.0, 0.0, 0.0, 0.0}
+                new double[]{pidConfig.kP, pidConfig.kI, pidConfig.kD, profile.maxVelocity, profile.maxAcceleration}
         );
 
         double[] initial = tunerForAllValues.readValue();
 
-
         //TODO make this use arjun ff
 
         ProfiledPIDFController controller = new ProfiledPIDFController(initial[0], initial[1], initial[2], 0, new TrapezoidProfile.Constraints(initial[3], initial[4]));
-        return new ProfiledPIDFCalculator(controller, tunerForAllValues);
+
+        var l = new ProfiledPIDFCalculator(controller, tunerForAllValues, self.generateDebugger());
+        self.registerLogLoop(l);
+        return l;
     }
 }
