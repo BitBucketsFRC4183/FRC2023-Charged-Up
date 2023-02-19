78ba161e23a08f94c122ddd9ff60522b2b29cad2
arjuncaputo <arjuncaputo2025@gmail.com>
2023-02-18 22:08:15 -0700

    alliance flip

---
+++ src/main/java/org/bitbuckets/vision/AllianceFlipUtil.java
@@ -0,0 +1,74 @@
+package org.bitbuckets.vision;
+
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+
+/**
+ * Utility functions for flipping from the blue to red alliance. By default, all translations and
+ * poses in are stored with the origin at the rightmost point on the blue
+ * alliance wall.
+ */
+public class AllianceFlipUtil {
+    /** Flips a translation to the correct side of the field based on the current alliance color. */
+    public static Translation2d applyTranslationFlip(Translation2d translation) {
+        if (shouldFlip()) {
+            return new Translation2d(VisionConstants.fieldLength - translation.getX(), translation.getY());
+        } else {
+            return translation;
+        }
+    }
+
+    /** Flips a rotation based on the current alliance color. */
+    public static Rotation2d applyRotationFlip(Rotation2d rotation) {
+        if (shouldFlip()) {
+            return new Rotation2d(-rotation.getCos(), rotation.getSin());
+        } else {
+            return rotation;
+        }
+    }
+
+    /** Flips a pose to the correct side of the field based on the current alliance color. */
+    public static Pose2d applyPoseFlip(Pose2d pose) {
+        if (shouldFlip()) {
+            return new Pose2d(
+                    VisionConstants.fieldLength - pose.getX(),
+                    pose.getY(),
+                    new Rotation2d(-pose.getRotation().getCos(), pose.getRotation().getSin()));
+        } else {
+            return pose;
+        }
+    }
+
+    /**
+     * Flips a trajectory state to the correct side of the field based on the current alliance color.
+     */
+    public static Trajectory.State applyTrajectoryFlip(Trajectory.State state) {
+        if (shouldFlip()) {
+            return new Trajectory.State(
+                    state.timeSeconds,
+                    state.velocityMetersPerSecond,
+                    state.accelerationMetersPerSecondSq,
+                    new Pose2d(
+                            VisionConstants.fieldLength - state.poseMeters.getX(),
+                            state.poseMeters.getY(),
+                            new Rotation2d(
+                                    -state.poseMeters.getRotation().getCos(),
+                                    state.poseMeters.getRotation().getSin())),
+                    -state.curvatureRadPerMeter);
+        } else {
+            return state;
+        }
+    }
+
+
+
+    private static boolean shouldFlip() {
+        return DriverStation.getAlliance() == Alliance.Red;
+    }
+}
+
