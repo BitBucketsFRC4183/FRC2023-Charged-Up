11d873ec40fcee4642632863031c301c43410c25
matt <yuuki@superyuuki.com>
2023-02-24 13:13:52 -0700

    fix arm subsystem not using loop

--- src/main/java/org/bitbuckets/robot/RobotContainer.java
+++ src/main/java/org/bitbuckets/robot/RobotContainer.java
@@ -39,13 +39,11 @@ public class RobotContainer {
     }
 
 
-    int i = 0;
 
     //Shouldn't need to do anything here
     public void teleopPeriodic() {
-        SmartDashboard.putNumber("debugIncrement", ++i);
         elevatorSubsystem.teleopPeriodic();
-        armSubsystem.teleopPeriodic();
+        armSubsystem.runLoop();
         macroSubsystem.teleopPeriodic();
 
 
