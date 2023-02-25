6361ea2418770169eb38427a755017e749b11dcf
arjuncaputo <arjuncaputo2025@gmail.com>
2023-02-25 13:13:18 -0700

    appa changes on main

--- src/main/java/org/bitbuckets/gripper/GripperConstants.java
+++ src/main/java/org/bitbuckets/gripper/GripperConstants.java
@@ -14,9 +14,9 @@ public interface GripperConstants {
     MotorConfig GRIPPER_CONFIG = new MotorConfig(1, 1, 1, false, true, 10, true, true, Optional.empty());
 
     DCMotorConfig DC_GRIPPER_CONFIG = new DCMotorConfig(0.01, new Matrix<>(SimpleMatrix.identity(0)));
-    PIDConfig GRIPPER_PID = new PIDConfig(1,0,0,0);
+    PIDConfig GRIPPER_PID = new PIDConfig(0.1,0,0,0);
 
     //Need to get this value, (0.1) is wrong
-    Double GRIPPER_SETPOINT_MOTOR_ROTATIONS = 1625.7742;
+    Double GRIPPER_SETPOINT_MOTOR_ROTATIONS = 1625.7742*1.5;
 
 }
