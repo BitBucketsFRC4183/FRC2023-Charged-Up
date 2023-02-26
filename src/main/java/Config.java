e5ab48d276891eeb45496bab19e2486c90604005
Nitish Bhamidipati <nitishbhamidipati@gmail.com>
2023-02-26 14:43:12 -0700

    moving all motorconfigs to one class

--- src/main/java/Config.java
+++ src/main/java/Config.java
@@ -1,5 +1,19 @@
+import org.bitbuckets.lib.ISetup;
+import org.bitbuckets.lib.hardware.IMotorController;
+import org.bitbuckets.lib.vendor.ctre.TalonDriveMotorSetup;
+import org.bitbuckets.lib.vendor.spark.SparkSetup;
+
 public interface Config {
 
     //DRIVE
+    ISetup<IMotorController> FRONT_LEFT_APPA = new TalonDriveMotorSetup();
+    ISetup<IMotorController> FRONT_RIGHT_APPA = new TalonDriveMotorSetup();
+    ISetup<IMotorController> BACK_LEFT_APPA = new TalonDriveMotorSetup();
+    ISetup<IMotorController> BACK_RIGHT_APPA = new TalonDriveMotorSetup();
+    ISetup<IMotorController> FRONT_LEFT = new TalonDriveMotorSetup();
+    ISetup<IMotorController> FRONT_RIGHT = new TalonDriveMotorSetup();
+    ISetup<IMotorController> BACK_LEFT = new TalonDriveMotorSetup();
+    ISetup<IMotorController> BACK_RIGHT = new TalonDriveMotorSetup();
+
 
 }
