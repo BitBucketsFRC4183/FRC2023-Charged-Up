7cabb86a00e8419c7d1b97747b7d773115780bf4
Ibrahim Bah <ib.bah2005@gmail.com>
2023-03-03 19:48:54 -0700

    PathConstraints so paths slow down during auto

--- src/main/java/org/bitbuckets/auto/AutoControlSetup.java
+++ src/main/java/org/bitbuckets/auto/AutoControlSetup.java
@@ -3,14 +3,12 @@ package org.bitbuckets.auto;
 import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
-import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import org.bitbuckets.lib.IProcess;
 import org.bitbuckets.lib.ISetup;
 import org.bitbuckets.odometry.IOdometryControl;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Supplier;
 
 public class AutoControlSetup implements ISetup<IAutoControl> {
 
@@ -30,12 +28,12 @@ public class AutoControlSetup implements ISetup<IAutoControl> {
             List<PathPlannerTrajectory> trajectory = PathPlanner.loadPathGroup("test-forwardright", new PathConstraints(1.25, 2.25), new PathConstraints(1.00, 1));
             List<PathPlannerTrajectory> trajectory1 = PathPlanner.loadPathGroup("taxi-right", new PathConstraints(4.0, 3.0));
             List<PathPlannerTrajectory> trajectory2 = PathPlanner.loadPathGroup("taxi-left", new PathConstraints(4.0, 3.0));
-            List<PathPlannerTrajectory> trajectory3 = PathPlanner.loadPathGroup("taxi-middle-dock", new PathConstraints(4.0, 3.0));
-            List<PathPlannerTrajectory> trajectory4 = PathPlanner.loadPathGroup("taxi-middle-dock-alt", new PathConstraints(4.0, 3.0));
-            List<PathPlannerTrajectory> trajectory5 = PathPlanner.loadPathGroup("SC1-CL1-BL", new PathConstraints(4.0, 3.0));
-            List<PathPlannerTrajectory> trajectory6 = PathPlanner.loadPathGroup("SC1-CL1-SC3-BL", new PathConstraints(4.0, 3.0));
-            List<PathPlannerTrajectory> trajectory8 = PathPlanner.loadPathGroup("SC9-CL4-BL", new PathConstraints(4.0, 3.0));
-            List<PathPlannerTrajectory> trajectory9 = PathPlanner.loadPathGroup("SC9-CL4-SC7-BL", new PathConstraints(4.0, 3.0));
+            List<PathPlannerTrajectory> trajectory3 = PathPlanner.loadPathGroup("taxi-middle-dock", new PathConstraints(4.0, 3.0), new PathConstraints(2.0, 1.0));
+            List<PathPlannerTrajectory> trajectory4 = PathPlanner.loadPathGroup("taxi-middle-dock-alt", new PathConstraints(4.0, 3.0), new PathConstraints(4.0, 3.5), new PathConstraints(2.0, 1.5));
+            List<PathPlannerTrajectory> trajectory5 = PathPlanner.loadPathGroup("SC1-CL1-BL", new PathConstraints(3.0, 2.5), new PathConstraints(4.0, 3.0), new PathConstraints(4.0, 3.0), new PathConstraints(2.0, 1.5));
+            List<PathPlannerTrajectory> trajectory6 = PathPlanner.loadPathGroup("SC1-CL1-SC3-BL", new PathConstraints(3.0, 2.0), new PathConstraints(4.0, 3.0), new PathConstraints(4.0, 3.0), new PathConstraints(2.0, 1.5));
+            List<PathPlannerTrajectory> trajectory8 = PathPlanner.loadPathGroup("SC9-CL4-BL", new PathConstraints(3.0, 2.5), new PathConstraints(4.0, 3.0), new PathConstraints(2.5, 2.0));
+            List<PathPlannerTrajectory> trajectory9 = PathPlanner.loadPathGroup("SC9-CL4-SC7-BL", new PathConstraints(2.5, 2.0), new PathConstraints(4.0, 3.0), new PathConstraints(4.0, 3.0), new PathConstraints(3.0, 2.5), new PathConstraints(2.5, 2.0));
 
 
             paths.add(trajectory);
@@ -50,7 +48,6 @@ public class AutoControlSetup implements ISetup<IAutoControl> {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
 
 
         return new AutoControl(paths, odometryControl);
