3b06a978cff1b86587114a19337201ae66dd74a4
DanielWhatsGood <daniel.changxu.wu@gmail.com>
2023-03-04 21:26:12 -0700

    added if statement that adds final time of stop events of last segment

--- src/main/java/org/bitbuckets/auto/AutoControl.java
+++ src/main/java/org/bitbuckets/auto/AutoControl.java
@@ -1,5 +1,6 @@
 package org.bitbuckets.auto;
 
+import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
 import edu.wpi.first.wpilibj.DriverStation;
 import org.bitbuckets.odometry.IOdometryControl;
@@ -25,7 +26,7 @@ public class AutoControl implements IAutoControl {
             return new AutoPathInstance(new ArrayList<>(), new HashMap<>(), new ArrayList<>(), whichOne, 0);
         }
 
-        var trajectoryGroup = trajectories.get(whichOne.index);
+        var segmentGroup = trajectories.get(whichOne.index);
 
         Map<String, Double> eventMap = new HashMap<>();
 
@@ -33,17 +34,31 @@ public class AutoControl implements IAutoControl {
         List<AutoPathInstance.SegmentTime> segmentTimes = new ArrayList<>();
 
 
-        for (int i = 0; i < trajectoryGroup.size(); i++) {
-            PathPlannerTrajectory segment = trajectoryGroup.get(i);
+        for (int i = 0; i < segmentGroup.size(); i++) {
+            PathPlannerTrajectory segment = segmentGroup.get(i);
 
             if (segment.getStartStopEvent().names.size() > 0) {
                 segmentTimes.add(new AutoPathInstance.SegmentTime(i, totalTime, true));
-                eventMap.put(segment.getStartStopEvent().names.get(0), totalTime);
+                for (String possibleEventName : segment.getStartStopEvent().names) {
+                    eventMap.put(possibleEventName, totalTime);
+                }
                 totalTime += segment.getStartStopEvent().waitTime;
             }
 
             segmentTimes.add(new AutoPathInstance.SegmentTime(i, totalTime, false));
             totalTime = totalTime + segment.getTotalTimeSeconds();
+
+            if (segment.getEndStopEvent().names.size() > 0 && segmentGroup.size() - 1 == i){
+                segmentTimes.add(new AutoPathInstance.SegmentTime(i, totalTime, true));
+
+
+                for (String possibleEventName : segment.getEndStopEvent().names) {
+                    eventMap.put(possibleEventName, totalTime);
+                }
+
+
+                totalTime += segment.getEndStopEvent().waitTime;
+            }
 
             for (PathPlannerTrajectory.EventMarker marker : segment.getMarkers()) {
                 for (String name : marker.names) {
@@ -53,26 +68,16 @@ public class AutoControl implements IAutoControl {
                 }
             }
 
-            //add the stop events and wait time
-
-            PathPlannerTrajectory.StopEvent event = segment.getEndStopEvent();
-
-            for (String possibleEventName : event.names) {
-                eventMap.put(possibleEventName, totalTime);
-            }
-
-            //add wait time
-            totalTime = totalTime + segment.getEndStopEvent().waitTime;
         }
 
         var transformedTrajectories = new ArrayList<PathPlannerTrajectory>();
-        for (var trajectory : trajectoryGroup) {
+        for (var trajectory : segmentGroup) {
             var transformTrajectory = PathPlannerTrajectory.transformTrajectoryForAlliance(trajectory, DriverStation.getAlliance());
             transformedTrajectories.add(transformTrajectory);
         }
 
 
-        var initialState = PathPlannerTrajectory.transformStateForAlliance(trajectoryGroup.get(0).getInitialState(), DriverStation.getAlliance());
+        var initialState = PathPlannerTrajectory.transformStateForAlliance(segmentGroup.get(0).getInitialState(), DriverStation.getAlliance());
         odometryControl.setPos(initialState.holonomicRotation, initialState.poseMeters);
         AutoPathInstance instance = new AutoPathInstance(transformedTrajectories, eventMap, segmentTimes, whichOne, totalTime);
 
