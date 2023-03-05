b8309bb39ad95574220d64b5e5a6e20baa1b52dc
Nitish Bhamidipati <nitishbhamidipati@gmail.com>
2023-03-04 20:49:10 -0700

    temp gripper control code

--- src/main/java/org/bitbuckets/arm/ArmControl.java
+++ src/main/java/org/bitbuckets/arm/ArmControl.java
@@ -111,5 +111,8 @@ public class ArmControl {
         return 1;
     }
 
-
+    //probably doesn't work but i need something to test rn
+    public boolean isErrorSmallEnough(double delta) {
+        return Math.abs(lowerArm.getError_mechanismRotations()) < delta && Math.abs(upperArm.getError_mechanismRotations()) < delta;
+    }
 }
