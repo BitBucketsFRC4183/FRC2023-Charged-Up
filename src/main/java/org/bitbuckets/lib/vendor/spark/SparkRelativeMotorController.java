c5de0204dfe74df5cfa3f12eb13a287ad8bdf637
Matt Lui <92664603+superyuuki@users.noreply.github.com>
2023-03-11 22:12:30 -0800

    more fixes to getAbsoluteEncoder

--- src/main/java/org/bitbuckets/lib/vendor/spark/SparkRelativeMotorController.java
+++ src/main/java/org/bitbuckets/lib/vendor/spark/SparkRelativeMotorController.java
@@ -122,6 +122,7 @@ public class SparkRelativeMotorController implements IMotorController, HasLogLoo
 
         lastControlMode = LastControlMode.VELOCITY;
         double rotationsPerMinute = velocity_encoderMetersPerSecond / getRotationsToMetersFactor() * 60.0;
+        sparkMax.getBusVoltage();
 
         sparkMaxPIDController.setReference(rotationsPerMinute, CANSparkMax.ControlType.kVelocity);
     }
