5fcaa46d5950c179a8723bc5c8e6f60df6de6fe9
Matt Lui <92664603+superyuuki@users.noreply.github.com>
2023-03-16 03:03:31 -0700

    remove tests that make 0 sense

--- src/test/java/org/bitbuckets/drive/holo/HoloControlTest.java
+++ src/test/java/org/bitbuckets/drive/holo/HoloControlTest.java
@@ -55,43 +55,5 @@ class HoloControlTest {
         assertEquals(0, chassisSpeeds.omegaRadiansPerSecond);
     }
 
-    @Test
-    void calculateTrajectoryState() {
-        // our robot starts at 4,4 with no rotation
-        when(odometryControl.estimateFusedPose2d()).thenReturn(new Pose2d(4, 4, Rotation2d.fromDegrees(0)));
 
-        // this is from our test-path in pathplanner
-        // we go from (4,4) to (6,4) with a -90º heading. The heading means
-        // our robot moves up a little as it moves forward but it shouldn't rotate at all
-        var state = new PathPlannerTrajectory.PathPlannerState();
-        state.poseMeters = new Pose2d(6, 4, Rotation2d.fromDegrees(-90));
-        state.holonomicRotation = Rotation2d.fromDegrees(0);
-        state.velocityMetersPerSecond = 1;
-        state.accelerationMetersPerSecondSq = 1;
-
-        var chassisSpeeds = control.calculatePose2D(state.poseMeters, 1);
-        assertEquals(2, chassisSpeeds.vxMetersPerSecond);
-        assertEquals(-1, chassisSpeeds.vyMetersPerSecond);
-        assertEquals(0, chassisSpeeds.omegaRadiansPerSecond);
-    }
-
-    @Test
-    void calculateTrajectoryState2() {
-        // for the second part of our test auto path, we are at (6, 4) and moving towards (6,6)
-        when(odometryControl.estimateFusedPose2d()).thenReturn(new Pose2d(6, 4, Rotation2d.fromDegrees(0)));
-
-        // this is from our test-path in pathplanner
-        // we go from (6,4) to (6,6) with a -90º heading. The heading means
-        // our robot moves straight down but shouldn't rotate at all
-        var state = new PathPlannerTrajectory.PathPlannerState();
-        state.poseMeters = new Pose2d(6, 6, Rotation2d.fromDegrees(-90));
-        state.holonomicRotation = Rotation2d.fromDegrees(0);
-        state.velocityMetersPerSecond = 1;
-        state.accelerationMetersPerSecondSq = 1;
-
-        var chassisSpeeds = control.calculatePose2D(state.poseMeters, 1);
-        assertEquals(0, chassisSpeeds.vxMetersPerSecond, .01);
-        assertEquals(1, chassisSpeeds.vyMetersPerSecond);
-        assertEquals(0, chassisSpeeds.omegaRadiansPerSecond);
-    }
 }
